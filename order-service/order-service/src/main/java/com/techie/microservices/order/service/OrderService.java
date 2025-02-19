0501a399f8b5094ecf784f0f4623a345c64ab379
Rohiid Naufal Juliardi <117732300+SandChik@users.noreply.github.com>
2025-02-20 05:20:12 +0700

    Add files via upload

---
+++ order-service/order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -0,0 +1,29 @@
+package com.techie.microservices.order.service;
+
+import com.techie.microservices.order.dto.OrderRequest;
+import com.techie.microservices.order.model.Order;
+import com.techie.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        //map order request to order object
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setQuantity(orderRequest.quantity());
+        order.setSkuCode(orderRequest.skuCode());
+        orderRepository.save(order);
+    }
+
+}
